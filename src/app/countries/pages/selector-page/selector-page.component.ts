090b8a9d677b2a885f544c97d817ed87e135102b
Andreea <andelenasamur@gmail.com>
2023-08-18 14:56:23 +0200

    borders selector

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interfaces';
-import { switchMap } from 'rxjs';
+import { filter, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -14,11 +14,12 @@ import { switchMap } from 'rxjs';
 export class SelectorPageComponent implements OnInit {
 
   public countriesByRegion: SmallCountry[] = [];
+  public borders: string[] = [];
 
   public myForm: FormGroup = this.fb.group({
     region:  ['', Validators.required],
     country: ['', Validators.required],
-    borders: ['', Validators.required],
+    border: ['', Validators.required],
   })
 
   constructor(
@@ -28,6 +29,7 @@ export class SelectorPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.onRegionChanged();
+    this.onCountryChanged();
   }
 
   get regions():Region[] {
@@ -37,11 +39,26 @@ export class SelectorPageComponent implements OnInit {
   onRegionChanged(): void {
     this.myForm.get('region')!.valueChanges
       .pipe(
+        tap( () => this.myForm.get('country')!.setValue('') ),
+        tap( () => this.borders = []),
         switchMap( region => this.countriesService.getCountriesByRegion(region))
       )
       .subscribe( countries => {
         this.countriesByRegion = countries;
       });
+  }
+
+  onCountryChanged():void {
+    this.myForm.get('country')!.valueChanges
+    .pipe(
+      tap( () => this.myForm.get('border')!.setValue('') ),
+      filter( (value: string ) => value.length > 0 ),
+      switchMap( alphaCode => this.countriesService.getCountryByAlphaCode(alphaCode))
+    )
+    .subscribe( country => {
+      console.log({borders: country.borders})
+      this.borders = country.borders;
+    });
   }
 
 }
